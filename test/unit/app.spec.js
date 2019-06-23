ee57a9061c526773085da1b40e107744c82c67e5
Cortlan Bainbridge <cortlan.bainbridge@gmail.com>
2019-06-23 08:55:27 -0500

    lint stuffs

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,13 +2,6 @@ import chai, {expect } from 'chai';
 import app from '../../src/app';
 
 describe('App', () => {
-    let req = {};
-    let res = {
-        sendCalledWith:'',
-        send: (argument) => {
-            this.sendCalledWith = argument
-        }
-    };
     it('should work', () => {
         chai.request(app)
         .get('/')
