4dbd3aaef7b49eed2b9790552f32b405f957e9b9
cortl <cortlan.bainbridge@gmail.com>
2019-08-15 15:30:44 -0500

    adding gitignore for build files, adding production config, adding bunyan json logging

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
 import indexRouter from './routes/index';
+import { logger } from './logger';
+import config from 'config';
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter());
+
+logger.info(`application started successfully on port: ${config.get('port')}`);
 
 export default app;
