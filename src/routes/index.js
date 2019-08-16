71eccb83d5f875316196e9d5121531742ffdbfdd
cortl <cortlan.bainbridge@gmail.com>
2019-08-16 10:48:16 -0500

    #6 adding stuff in readme for application logging

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,6 @@ import { logger } from '../logger';
 const router = () => {
   const router = express.Router();
   router.get('/', (_req, res) => res.send('Hello World!'));
-  router.get('/log/:message', (req, res) => {
-    logger().info(`message received: ${req.params.message}`);
-    res.status(200).send(req.params.message);
-  });
   return router;
 };
 
