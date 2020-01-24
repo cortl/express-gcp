3ae2102501f3f114f9bd4e7845f02fbbf44f47b6
cortl <cortlan.bainbridge@gmail.com>
2020-01-24 08:48:19 -0600

    updating dependencies and adding helmet

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import indexRouter from './routes/index';
+import helmet from 'helmet';
 import config from 'config';
+
+import indexRouter from './routes/index';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use('/', indexRouter());
